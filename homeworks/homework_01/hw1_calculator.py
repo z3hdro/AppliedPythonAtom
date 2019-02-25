964f106cb354d142f72baf7ee557421acedd21e6
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-02-25 13:09:15 +0300

    [Lecture01] Start homework pushing

---
+++ homeworks/homework_01/hw1_calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def calculator(x, y, operator):
+    '''
+    Простенький калькулятор в прямом смысле. Работает c числами
+    :param x: первый агрумент
+    :param y: второй аргумент
+    :param operator: 4 оператора: plus, minus, mult, divide
+    :return: результат операции или None, если операция не выполнима
+    '''
+    raise NotImplementedError
