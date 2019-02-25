edc5028ec3e44c8bf8ce7d88986c2444677c708f
Kinetik <kinetikm@gmail.com>
2019-02-25 21:46:13 +0300

    [Lecture01] Add hw01_part 2

--- tests/test_hw_01.py
+++ tests/test_hw_01.py
@@ -5,6 +5,7 @@ import os
 
 from utils.file_processors import PickleFileProcessor
 from homeworks.homework_01.hw1_calculator import calculator
+from homeworks.homework_01.hw1_calcadv import advanced_calculator
 
 
 def load_test_data(func_name):
@@ -36,4 +37,16 @@ def test_calculator():
 
 
 def test_string_calculator():
-    return True
+    data = load_test_data("calcadv")
+    try:
+        advanced_calculator("plus")
+    except NotImplementedError:
+        return True
+    for arguments in data:
+        input_string = arguments[0]
+        result = arguments[1]
+        if result is None:
+            assert advanced_calculator(input_string) is None
+            continue
+        output = advanced_calculator(input_string) - result
+        assert output / abs(result + 0.00001) < 0.005
