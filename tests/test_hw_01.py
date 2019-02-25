67fa530f00a5e6d1b4d46d9ba870f12f28962834
Kinetik <kinetikm@gmail.com>
2019-02-25 16:08:40 +0300

    [Lecture01] Fixed tests

---
+++ tests/test_hw_01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+
+from utils.file_processors import PickleFileProcessor
+from homeworks.homework_01.hw1_calculator import calculator
+
+
+def load_test_data(func_name):
+    file_processor = PickleFileProcessor()
+    test_filename = os.path.basename(__file__)
+    test_filename = os.path.splitext(test_filename)[0]
+    test_filename = os.path.join("tests/tests_data",
+                                 test_filename + "_" + func_name + ".ini.pkl")
+    output = file_processor.read_file(test_filename)
+    return output
+
+
+def test_calculator():
+    data = load_test_data("calculator")
+    try:
+        calculator(1, 1, "plus")
+    except NotImplementedError:
+        return True
+    for arguments in data:
+        x = arguments[0]
+        y = arguments[1]
+        oper = arguments[2]
+        result = arguments[3]
+        if result is None:
+            assert calculator(x, y, oper) is None
+            continue
+        assert abs(calculator(x, y, oper) - result) / abs(result + 0.00001) < 0.005
--- tests/test_lecture_01.py
+++ tests/test_lecture_01.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pytest
-import gzip
-
+from utils.file_processors import TarFileProcessor
 from lectures.lecture_01.scripts.submission import calculator
 
 
-@pytest.fixture
 def load_test_data():
     import os
-    testname = os.path.basename(__file__)
-    testname = os.path.splitext(testname)[0]
-    with gzip.open(os.path.join("tests/tests_data",
-                                testname + ".tar.gz"), 'rb') as f:
-        output = f.read()
+    tarfile_processor = TarFileProcessor()
+    test_filename = os.path.basename(__file__)
+    test_filename = os.path.splitext(test_filename)[0]
+    test_filename = os.path.join("tests/tests_data",
+                                 test_filename + ".ini.gz")
+    output = tarfile_processor.read_file(test_filename)
     return output
 
 
-def test_calculator(load_test_data):
-    params = load_test_data
-    for arguments, result in params:
+def test_calculator():
+    params = load_test_data()
+    for arguments in params:
+        arguments = arguments.split()
         x = int(arguments[0])
         y = int(arguments[1])
         operation = arguments[2]
-        assert calculator(x, y, operation) == int(result)
+        assert calculator(x, y, operation) == int(arguments[3])
