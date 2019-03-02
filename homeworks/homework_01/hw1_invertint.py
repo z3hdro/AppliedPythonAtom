2b85fa79512dcf9f4fbd73397553a25b9e8833aa
unknown <eldarjumagliev@gmail.com>
2019-03-02 19:49:00 +0300

    HW

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,10 +3,24 @@
 
 
 def reverse(number):
+   
     '''
     Метод, принимающий на вход int и
     возвращающий инвертированный int
     :param number: исходное число
     :return: инвертированное число
     '''
-    raise NotImplementedError
+     if number >= 0:
+        flag = 0
+    else:
+        flag = 1
+    x = str(number)
+    if flag is 0:
+        y = x[::-1]
+        z = int(y)
+        return z
+    else:
+        y = x[1:]
+        v = y[::-1]
+        z = int(v)
+        return -1 * z
