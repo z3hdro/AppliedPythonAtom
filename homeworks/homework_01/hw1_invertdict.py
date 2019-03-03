fc3f95bf5796110967dd8afb7d06f36423dc16d8
unknown <eldarjumagliev@gmail.com>
2019-03-03 13:28:16 +0300

    invertdict

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -9,16 +9,4 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    new_dict = {}
-    for key, value in source_dict.items():
-        for i in range(0, len(value), 1):
-            if not (value[i] in new_dict):
-                new_dict[value[i]] = key
-            elif new_dict[value[i]] is list() or tuple():
-                new_dict[value[i]].append(key)
-            elif new_dict[value[i]] is set():
-                new_dict[value[i]].add(key)
-            else:
-                new_dict[value[i]] = [new_dict[value[i]]]
-                new_dict[value[i]].append(key)
-    return new_dict
+    raise NotImplementedError
