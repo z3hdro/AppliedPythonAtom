65d0a62199fd26ec88aa937e8880e949f772f99e
unknown <eldarjumagliev@gmail.com>
2019-03-03 13:03:04 +0300

    brseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -12,9 +12,9 @@ def is_bracket_correct(input_string):
     '''
     s = input_string
     while "[]" in s or "()" in s or "{}" in s:
-        s = s.replace("[]","")
-        s = s.replace("()","")
-        s = s.replace("{}","")
+        s = s.replace("[]", "")
+        s = s.replace("()", "")
+        s = s.replace("{}", "")
     if s != "":
         return False
     else:
