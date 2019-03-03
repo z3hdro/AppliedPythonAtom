6f1e39e17bd1ff8d16240ad767e2a5257e0d9021
unknown <eldarjumagliev@gmail.com>
2019-03-03 23:03:36 +0300

    invertdict

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -2,6 +2,20 @@
 # coding: utf-8
 
 
+def inside(value):
+    res = []
+    if isinstance(value, (tuple, set, list)):
+        for i in value:
+            if isinstance(value, (tuple, set, list)):
+                for j in inside(i):
+                    res.append(j)
+            else:
+                res.append(i)
+    else:
+        res = [value]
+    return res
+
+
 def invert_dict(source_dict):
     '''
     Функция которая разворачивает словарь, т.е.
@@ -9,4 +23,16 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    raise NotImplementedError
+    if type(source_dict) != dict:
+        return source_dict
+    new_dict = {}
+    for pair in source_dict.items():
+        for value in inside(pair[1]):
+            if value not in new_dict:
+                new_dict[value] = pair[0]
+            elif type(new_dict[value]) is list:
+                new_dict[value].append(pair[0])
+            else:
+                new_dict[value] = [new_dict[value]]
+                new_dict[value].append(pair[0])
+    return new_dict
