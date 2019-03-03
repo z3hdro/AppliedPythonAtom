047bca3bc021aa76a8c6845f591302a2d288e348
unknown <eldarjumagliev@gmail.com>
2019-03-03 17:53:14 +0300

    det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+def min(matrix, j):
+        return matrix[j][0], [row[1:] for row in matrix[:j] + matrix[j + 1:]]
+    
+    
+def det(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    ans = 0
+    for j in range(len(matrix)):
+        A, M = min(matrix, j)
+        ans += ((-1) ** j) * A * det(M)
+    return ans
+
 
 def calculate_determinant(list_of_lists):
     '''
@@ -11,4 +24,7 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    for i in list_of_lists:
+        if len(i) != len(list_of_lists):
+            return None
+    return(det(list_of_lists))
