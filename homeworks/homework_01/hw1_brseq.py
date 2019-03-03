5e6ea5203d9c5478dc58efb08871544670fa21e1
unknown <eldarjumagliev@gmail.com>
2019-03-03 14:17:18 +0300

    brseq

--- homeworks/homework_01/hw1_brseq.py
+++ homeworks/homework_01/hw1_brseq.py
@@ -3,6 +3,13 @@
 
 
 def is_bracket_correct(input_string):
+    '''
+    Метод проверяющий является ли поданная скобочная
+     последовательность правильной (скобки открываются и закрываются)
+     не пересекаются
+    :param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
+    :return: True or False
+    '''
     s = input_string
     while "[]" in s or "()" in s or "{}" in s:
         s = s.replace("[]", "")
