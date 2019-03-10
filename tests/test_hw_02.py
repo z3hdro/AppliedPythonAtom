78116df9c8f64e27663a1eb05cd94a9ddc3f5124
Zarina Sayfullina <z.sayfullina@corp.mail.ru>
2019-03-10 19:55:24 +0300

    added test for hw2 table

--- tests/test_hw_02.py
+++ tests/test_hw_02.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 import os
+import tempfile
+import subprocess
 
 from utils.file_processors import PickleFileProcessor
 
@@ -82,4 +84,23 @@ def test_vk_poster():
                 assert ans == vk_poster.user_read_post(*params)
             elif action == 'get_most_popular_posts':
                 assert ans == vk_poster.get_most_popular_posts(*params)
+    return True
+
+def test_table():
+    data = load_test_data("table")
+
+    for test_data, test_out in data:
+        fd, path = tempfile.mkstemp()
+        try:
+            with os.fdopen(fd, 'wb') as tmp:
+                tmp.write(test_data)
+
+            out, err = subprocess.Popen(['python', 'homeworks/homework_02/table.py', path], stdout=subprocess.PIPE).communicate()
+            assert test_out.strip() == out.decode('utf8').strip()
+        finally:
+            os.remove(path)
+
+    # Not Found
+    out, err = subprocess.Popen(['python', 'homeworks/homework_02/table.py', 'path'], stdout=subprocess.PIPE).communicate()
+    assert 'Файл не валиден' == out.decode('utf8').strip()
     return True
