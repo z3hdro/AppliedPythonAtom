1034ca7628a63d827060038438be5751e7b6c667
Kinetik <kinetikm@gmail.com>
2019-03-13 23:34:45 +0300

    [Lecture03] End lrucache testing

--- tests/test_hw_03.py
+++ tests/test_hw_03.py
@@ -236,38 +236,70 @@ def test_lrucache_03():
         return True
 
     t_start = time.time()
-    res = get_sq(1)
+    get_sq(1)
     assert time.time() - t_start > 0.5
     t_start = time.time()
-    res = get_sq(1)
+    get_sq(1)
     assert time.time() - t_start < 0.5
     l = [5, 6, 7]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start > 0.5
     l = [1, 5, 6, 7]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start < 0.5
     l = [7, 5, 6, 1]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start < 0.5
     l = [15]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start > 0.5
     l = [1, 6, 5, 15]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start < 0.5
     l = [7]
     for i in l:
         t_start = time.time()
-        res = get_sq(i)
+        get_sq(i)
         assert time.time() - t_start > 0.5
+
+
+def test_lrucache_04():
+    my_global_vars = {}
+    try:
+        @LRUCacheDecorator(maxsize=3, ttl=10)
+        def get_sq(s):
+            time.sleep(2)
+            global my_global_vars
+            my_global_vars[s] = s ** 2
+            return my_global_vars[s]
+    except NotImplementedError:
+        return True
+
+    get_sq(3)
+    t_start = time.time()
+    get_sq(3)
+    assert time.time() - t_start < 2
+    assert my_global_vars[3] == get_sq(3) == 9
+
+    for i in my_global_vars:
+        my_global_vars[i] += 1
+
+    t_start = time.time()
+    get_sq(3)
+    assert time.time() - t_start < 2
+    assert my_global_vars[3] != get_sq(3)
+
+    time.sleep(10)
+    t_start = time.time()
+    get_sq(3)
+    assert my_global_vars[3] == get_sq(3)
