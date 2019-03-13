a509be8bdb9ca7709f425ef344f7da61f15032bc
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-03-13 20:44:16 +0300

    [Lecture03] HW_03 start testing

---
+++ tests/test_hw_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from homeworks.homework_03.hw3_hashmap import HashMap
+from homeworks.homework_03.hw3_hashset import HashSet
+from homeworks.homework_03.hw3_lrucache import LRUCacheDecorator
+from homeworks.homework_03.hw3_testing import MockOrdinaryFileWorker
