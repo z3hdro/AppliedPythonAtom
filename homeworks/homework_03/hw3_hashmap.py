9594a0b6ab170e698c9b0ffc7662c8eaded00d93
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-03-13 11:02:00 +0300

    [Lecture03] Start homework pushing

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -16,46 +16,63 @@ class HashMap:
             '''
 
         def get_key(self):
-            #py TODO возвращаем ключ
+            # TODO возвращаем ключ
+            raise NotImplementedError
 
         def get_value(self):
             # TODO возвращаем значение
+            raise NotImplementedError
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
+            raise NotImplementedError
 
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         '''
+        raise NotImplementedError
 
     def get(self, key, default_value=None):
-        #TODO метод get, возвращающий значение,
-        # если оно присутствует, иначе default_value
+        # TODO метод get, возвращающий значение,
+        #  если оно присутствует, иначе default_value
+        raise NotImplementedError
 
     def put(self, key, value):
-        #TODO метод put, кладет значение по ключу,
-        # в случае, если ключ уже присутствует он его заменяет
+        # TODO метод put, кладет значение по ключу,
+        #  в случае, если ключ уже присутствует он его заменяет
+        raise NotImplementedError
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
+        raise NotImplementedError
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
+        raise NotImplementedError
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
+        raise NotImplementedError
 
     def values(self):
         # TODO Должен возвращать итератор значений
+        raise NotImplementedError
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
+        raise NotImplementedError
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
+        raise NotImplementedError
+
+    def resize(self):
+        # TODO Время от времени нужно ресайзить нашу хешмапу
+        raise NotImplementedError
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
+        raise NotImplementedError
