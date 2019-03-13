316a668f8a74b2a01b13645c89f45ae29f9dc621
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-03-13 11:25:28 +0300

    [Lecture03] HW 02 pushed without testing

---
+++ homeworks/homework_03/hw3_lrucache.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class LRUCacheDecorator:
+
+    def __init__(self, func, n):
+        # TODO инициализация декоратора
+        #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
+        raise NotImplementedError
+
+    def __call__(self, *args, **kwargs):
+        # TODO вызов функции
+        raise NotImplementedError
