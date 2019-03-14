cdb835fb89c9fa4926deb6a677942e3010e92a05
Ilya Lysko <ilya.lysko@gmail.com>
2019-03-14 12:26:37 +0300

    [Lecture 03]: event_stats, groupping_anagramms and revert_linked_list templates.

---
+++ homeworks/homework_03/hw3_revert_linked_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Класс LLEntity вам не нужен -- это просто болванка, чтобы вы понимали, что такое head
+#
+# class LLEntity:
+#     def __init__(self, value, next, prev):
+#         """
+#         Entity (or node) for doubly linked list
+#         :param value: object
+#         :param next: LLEntity
+#         :param prev: LLEntity
+#         """
+#         self.value = ...
+#         self.next = ...
+#         self.prev = ...
+
+
+def revert_linked_list(head):
+    """
+    A <-> B <-> C should become: C <-> B <-> A
+    :param head: LLNode
+    :return: new_head: LLNode
+    """
+    # TODO: реализовать функцию
+    raise NotImplementedError
