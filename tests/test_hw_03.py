83aa669b1cdb1fc9c58f30f19e89c1513b35ff02
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-03-16 16:46:18 +0300

    [Lecture03] Small test fix

--- tests/test_hw_03.py
+++ tests/test_hw_03.py
@@ -206,7 +206,7 @@ def test_hashset_05():
     assert len(hashset_3) == len(entries)
 
 
-def test_hashset_05():
+def test_hashset_06():
     try:
         hashset = HashSet()
     except NotImplementedError:
@@ -221,7 +221,7 @@ def test_hashset_05():
     hashset_3 = hashset.intersect(hashset_2)
     assert hashset_3 is not hashset
     assert hashset_3 is not hashset_2
-    assert len(hashset_3) == len(entries)
+    assert len(hashset_3) == 0
 
 
 def test_lrucache_01():
