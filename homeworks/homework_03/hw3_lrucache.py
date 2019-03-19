4dc0ac3d83c7734e7b6269211d5021dc91437912
unknown <eldarjumagliev@gmail.com>
2019-03-19 19:11:58 +0300

    lrucache 0.1

--- homeworks/homework_03/hw3_lrucache.py
+++ homeworks/homework_03/hw3_lrucache.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import time
+from collections import OrderedDict
+
 
 class LRUCacheDecorator:
 
@@ -12,8 +15,36 @@ class LRUCacheDecorator:
         '''
         # TODO инициализация декоратора
         #  https://www.geeksforgeeks.org/class-as-decorator-in-python/
-        raise NotImplementedError
+        self.size = maxsize
+        self.ttl = ttl
+        self.cache = OrderedDict()
+        self.origin = time.time()
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, func):
         # TODO вызов функции
-        raise NotImplementedError
+        def wrapper(*args, **kwargs):
+            col = set()
+            for i in args:
+                col.add(i)
+            for key, value in kwargs.items():
+                col.add(key)
+                col.add(value)
+            data = tuple(col)
+            if self.ttl is None or time.time() - self.origin <= self.ttl:
+                if data in self.cache:
+                    if len(self.cache) == self.size:
+                        temp = self.cache[data]
+                        del self.cache[data]
+                        self.cache[data] = temp
+                    return self.cache[data]
+                else:
+                    if len(self.cache) == self.size:
+                        del self.cache[list(self.cache.keys())[0]]
+                    self.cache[data] = func(*args, **kwargs)
+                    return func(*args, **kwargs)
+            else:
+                self.cache.clear()
+                self.origin = time.time()
+                self.cache[data] = func(*args, **kwargs)
+                return func(*args, **kwargs)
+        return wrapper
