3bbb8cba43d1ac75d51ae206a6130cab45435a75
Zarina Sayfullina <z.sayfullina@corp.mail.ru>
2019-03-21 11:15:21 +0300

    fix build

--- homeworks/reader.py
+++
@@ -1,8 +0,0 @@
-def read_data(filename):
-    for coding in ('utf8', 'cp1251', 'utf16le', 'utf-16be'):
-        try:
-            with open(filename, encoding=coding) as f:
-                return f.read()
-        except UnicodeDecodeError:
-            continue
-    raise ValueError
