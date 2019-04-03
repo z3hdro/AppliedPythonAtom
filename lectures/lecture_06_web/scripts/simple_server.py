35a8ce4f2c06dfa64528c6c9c656d3c859c18bda
Баранов Михаил <mikhail.baranov@corp.mail.ru>
2019-04-03 09:26:25 +0300

    [Lecture06Web] Pep fix

--- lectures/lecture_06_web/scripts/simple_server.py
+++ lectures/lecture_06_web/scripts/simple_server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
- 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
@@ -10,7 +9,7 @@ class SimpleReqHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
- 
+
         message = "Hello world!"
         self.wfile.write(bytes(message, "utf8"))
         return
@@ -18,7 +17,7 @@ class SimpleReqHandler(BaseHTTPRequestHandler):
 
 def run():
     print('starting server...')
- 
+
     server_address = ('127.0.0.1', 8081)
     httpd = HTTPServer(server_address, SimpleReqHandler)
     print('running server...')
