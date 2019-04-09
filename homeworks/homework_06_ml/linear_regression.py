d52ced20cfd43fa3347468882c9dbd4a9d55c00d
Ilya Lysko <ilya.lysko@gmail.com>
2019-04-09 13:38:40 +0300

    [Lecture 06 ML] homework

---
+++ homeworks/homework_06_ml/linear_regression.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class LinearRegression:
+    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+        """
+        :param lambda_coef: constant coef for gradient descent step
+        :param regulatization: regularizarion type ("L1" or "L2") or None
+        :param alpha: regularizarion coefficent
+        """
+        pass
+
+    def fit(self, X_train, y_train):
+        """
+        Fit model using gradient descent method
+        :param X_train: training data
+        :param y_train: target values for training data
+        :return: None
+        """
+        pass
+
+    def predict(self, X_test):
+        """
+        Predict using model.
+        :param X_test: test data for predict in
+        :return: y_test: predicted values
+        """
+        pass
+
+    def get_weights(self):
+        """
+        Get weights from fitted linear model
+        :return: weights array
+        """
+        pass
---
+++ homeworks/homework_06_ml/metrics.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+
+
+def mse(y_true, y_hat, derivative=False):
+    """
+    Mean squared error regression loss
+    :param y_true: vector of truth (correct) target values
+    :param y_hat: vector of estimated target values
+    :return: loss
+    """
+    pass
+
+
+def mae(y_true, y_hat):
+    """
+    Mean absolute error regression loss
+    :param y_true: vector of truth (correct) target values
+    :param y_hat: vector of estimated target values
+    :return: loss
+    """
+    pass
+
+
+def r2_score(y_true, y_hat):
+    """
+    R^2 regression loss
+    :param y_true: vector of truth (correct) target values
+    :param y_hat: vector of estimated target values
+    :return: loss
+    """
+    pass
