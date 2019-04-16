e5afe5187b84688cc00f22d4c66b9593fa4920bb
amigoml <amir14111@yandex.ru>
2019-04-16 21:25:27 +0300

    Add files via upload

---
+++ homeworks/homework_07_ml/metrics.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+
+
+def logloss(y_true, y_pred):
+    """
+    logloss
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated probabilities
+    :return: loss
+    """
+    pass
+
+
+def accuracy(y_true, y_pred):
+    """
+    Accuracy
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    pass
+
+
+def presicion(y_true, y_pred):
+    """
+    presicion
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    pass
+
+
+def recall(y_true, y_pred):
+    """
+    presicion
+    :param y_true: vector of truth (correct) class values
+    :param y_hat: vector of estimated class values
+    :return: loss
+    """
+    pass
+
+
+def roc_auc(y_true, y_pred):
+    """
+    roc_auc
+    :param y_true: vector of truth (correct) target values
+    :param y_hat: vector of estimated probabilities
+    :return: loss
+    """
+    pass
