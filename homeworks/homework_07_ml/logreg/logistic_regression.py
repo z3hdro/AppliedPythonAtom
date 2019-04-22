9bc9536ce314df36f7c57cd09081ab628941027e
unknown <eldarjumagliev@gmail.com>
2019-04-22 22:58:14 +0300

    nujno bolshe kofe

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -3,17 +3,22 @@
 
 
 import numpy as np
+from sklearn.metrics import log_loss
 
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(self,n_iter=100 , lambda_coef=1, accuracy=0.00001, regulatization=None, alpha=0.5):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        pass
+        self.labmda_coef = lambda_coef
+        self.regulatization = regulatization
+        self.alpha = alpha
+        self.n_iter = n_iter
+        self.accuracy = accuracy
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +27,31 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        if not X_train.shape[0] == y_train.shape[0]:
+            print('Different shapes')
+        X_const = np.hstack(((np.ones((X_train.shape[0], 1))), X_train))
+        n = X_const.shape[0]
+        m = X_const.shape[1]
+        self.w = np.random.randn(m).reshape(-1, 1)
+        y_pred = (self.predict_proba(X_train)[:, 1]).reshape(-1, 1)
+        init = log_loss(y_train, y_pred)
+        for i in range(self.n_iter):
+            if self.regularizarion == 'L2':
+                derivative = 2 * self.alpha * self.w 
+                derivative[0] = 0
+            elif self.regularizarion == 'L1':
+                derivative = self.alpha * np.ones(m)
+                derivative[0] = 0
+            else:
+                derivative = np.zeros(m) 
+            self.w -= -self.labmda_coef * (X_const.T @ (y_pred - y_train.reshape(-1, 1)) + derivative) / n
+            y_pred = (self.predict_proba(X_train)[:, 1]).reshape(-1, 1)
+            next = log_loss(y_train, y_pred)
+            if np.abs(next - init) < self.accuracy:
+                break
+            init = next
+
+
 
     def predict(self, X_test):
         """
@@ -30,7 +59,9 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        X_test = np.hstack(((np.ones((X_test.shape[0], 1))), X_test))
+        y_pred = 1 / (1 + np.exp(X_test @ self.w))
+        return (y_pred > 0.5).astype('int64')
 
     def predict_proba(self, X_test):
         """
@@ -38,11 +69,13 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        X_test = np.hstack(((np.ones((X_test.shape[0], 1))), X_test))
+        y_test = 1 / (1 + np.exp(X_test @ self.w))
+        return np.hstack((1 - y_test, y_test))
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        return self.w
--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -12,8 +12,11 @@ def logloss(y_true, y_pred):
     :param y_pred: vector of estimated probabilities
     :return: loss
     """
-    sm = np.sum((y_true * np.log(y_pred)) + (1 - y_true) * np.log(1 - y_pred))
-    return -1 * (sm / len(y_true))
+    if not y_true.shape == y_pred.shape:
+        print('Different shapes')
+    else:
+        sm = np.sum((y_true * np.log(y_pred)) + (1 - y_true) * np.log(1 - y_pred))
+        return -1 * (sm / len(y_true))
 
 
 def accuracy(y_true, y_pred):
@@ -23,11 +26,14 @@ def accuracy(y_true, y_pred):
     :param y_pred: vector of estimated class values
     :return: loss
     """
-    m = []
-    for i in range(len(y_true)):
-        if y_true[i] == y_pred[i]:
-            m.append(1)
-    return len(m) / len(y_true)
+    if not y_true.shape == y_pred.shape:
+        print('Different shapes')
+    else:
+        m = []
+        for i in range(len(y_true)):
+            if y_true[i] == y_pred[i]:
+                m.append(1)
+        return len(m) / len(y_true)
 
 
 def presicion(y_true, y_pred):
@@ -37,12 +43,15 @@ def presicion(y_true, y_pred):
     :param y_pred: vector of estimated class values
     :return: loss
     """
-    m = []
-    for i in range(len(y_true)):
-        if (y_pred[i] == 1) and (y_true[i] == 1):
-            m.append(1)
-    full = np.count_nonzero(y_pred)
-    return len(m) / full
+    if not y_true.shape == y_pred.shape:
+        print('Different shapes')
+    else:
+        m = []
+        for i in range(len(y_true)):
+            if (y_pred[i] == 1) and (y_true[i] == 1):
+                m.append(1)
+        full = np.count_nonzero(y_pred)
+        return len(m) / full
 
 
 def recall(y_true, y_pred):
@@ -52,12 +61,15 @@ def recall(y_true, y_pred):
     :param y_pred: vector of estimated class values
     :return: loss
     """
-    m = []
-    for i in range(len(y_true)):
-        if (y_pred[i] == 1) and (y_true[i] == 1):
-            m.append(1)
-    full = np.count_nonzero(y_true)
-    return len(m) / full
+    if not y_true.shape == y_pred.shape:
+        print('Different shapes')
+    else:
+        m = []
+        for i in range(len(y_true)):
+            if (y_pred[i] == 1) and (y_true[i] == 1):
+                m.append(1)
+        full = np.count_nonzero(y_true)
+        return len(m) / full
 
 
 def roc_auc(y_true, y_pred):
@@ -67,37 +79,39 @@ def roc_auc(y_true, y_pred):
     :param y_pred: vector of estimated probabilities
     :return: loss
     """
-
-    t = []
-    k = []
-    ones = []
-    zeros = []
-    result = []
-    step = 1 / len(y_true)
-    m = 0
-    while m <= 1:
-        t.append(m)
-        m += step
-    for l in t:
-        for j in range(len(y_pred)):
-            if y_pred[j] > l:
-                k.append(1)
-            else:
-                k.append(0)
-        y_hat = np.array(k)
-        for i in range(len(y_true)):
-            if (y_hat[i] == 1) and (y_true[i] == 1):
-                ones.append(1)
-        ones_length = np.count_nonzero(y_true)
-        TPR = len(ones) / ones_length
-        for i in range(len(y_true)):
-            if (y_hat[i] == 0) and (y_true[i] == 0):
-                zeros.append(0)
-        zeros_length = len(y_hat) - ones_length
-        FPR = 1 - (len(zeros) / zeros_length)
-        answer = (1 + TPR - FPR) / 2
-        result.append(answer)
-        k.clear()
-        ones.clear()
-        zeros.clear()
-    return max(result)
+    if not y_true.shape == y_pred.shape:
+        print('Different shapes')
+    else:
+        t = []
+        k = []
+        ones = []
+        zeros = []
+        result = []
+        step = 1 / len(y_true)
+        m = 0
+        while m <= 1:
+            t.append(m)
+            m += step
+        for l in t:
+            for j in range(len(y_pred)):
+                if y_pred[j] > l:
+                    k.append(1)
+                else:
+                    k.append(0)
+            y_hat = np.array(k)
+            for i in range(len(y_true)):
+                if (y_hat[i] == 1) and (y_true[i] == 1):
+                    ones.append(1)
+            ones_length = np.count_nonzero(y_true)
+            TPR = len(ones) / ones_length
+            for i in range(len(y_true)):
+                if (y_hat[i] == 0) and (y_true[i] == 0):
+                    zeros.append(0)
+            zeros_length = len(y_hat) - ones_length
+            FPR = 1 - (len(zeros) / zeros_length)
+            answer = (1 + TPR - FPR) / 2
+            result.append(answer)
+            k.clear()
+            ones.clear()
+            zeros.clear()
+        return max(result)
