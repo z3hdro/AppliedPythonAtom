84dc1781a34b6c0403e852c21fe351b73ee968a7
unknown <eldarjumagliev@gmail.com>
2019-05-01 13:08:14 +0300

    knn

--- homeworks/homework_08_ml/ml_classes/knns.py
+++ homeworks/homework_08_ml/ml_classes/knns.py
@@ -12,7 +12,7 @@ class KNNRegressor:
         Конструктор
         :param n: число ближайших соседей, которые используются
         '''
-        raise NotImplementedError
+        self.n = n
 
     def fit(self, X, y):
         '''
@@ -20,26 +20,26 @@ class KNNRegressor:
         :param y: целевая переменная, матрица размерности (num_obj, 1)
         :return: None
         '''
-        raise NotImplementedError
+        self.X = X
+        self.y = y
 
     def predict(self, X):
         '''
         :param X: выборка, на которой хотим строить предсказания (num_test_obj, num_features)
         :return: вектор предсказаний, матрица размерности (num_test_obj, 1)
         '''
-
-        raise NotImplementedError
-
         y = []
         assert len(X.shape) == 2
         for t in X:
             # Посчитаем расстояние от всех элементов в тренировочной выборке
             # до текущего примера -> результат - вектор размерности трейна
             # TODO d =
+            d = np.linalg.norm(self.X - t, axis = 1)
             # Возьмем индексы n элементов, расстояние до которых минимально
             # результат -> вектор из n элементов
             # TODO idx =
+            idx = pn.argsort(d)[:self.n]
             # TODO
-            prediction = None
+            prediction = np.average(self.y[idx])
             y.append(prediction)
-        return y
+        return np.array(y)
